6e4f9c06cb50e4d1f9901bf023fdc72a6018c304
Hamze GHALEBI <ghalebi@gmail.com>
2025-09-06 19:36:30 +0200

    backward forward

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
+use std::result;
 use rmcp::handler::server::tool::{ ToolRouter};
 use rmcp::model::{CallToolResult, Content, Implementation, ProtocolVersion, ServerCapabilities, ServerInfo};
-use rmcp::schemars;
-use rmcp::schemars::JsonSchema;
 use rmcp::transport::stdio;
-use rmcp::{tool, tool_handler, tool_router, ErrorData, ServerHandler, ServiceExt};
-use serde::Deserialize;
-use std::fs;
-use tracing::info;
-
-
-
+use rmcp::{schemars, tool, tool_handler, tool_router, ErrorData, ServerHandler, ServiceExt};
 use rmcp::handler::server::wrapper::Parameters;
-
+use serde::Deserialize;
+use tracing::info;
+use serde::Serialize;
+use serde_json::to_string;
 const ROVER_IP: &str = "192.168.4.1";
 
 fn send_command(json: &str) -> String {
@@ -20,6 +16,56 @@ fn send_command(json: &str) -> String {
 	match reqwest::blocking::get(&url) {
 		Ok(response) => response.text().unwrap_or("No response".to_string()),
 		Err(e) => format!("Error: {}", e),
+	}
+}
+#[derive(Serialize)]
+pub struct GoForward{
+	#[serde(rename = "T")]
+	command: u8, // Always 1
+	#[serde(rename = "L")]
+	left_speed: f32,
+	#[serde(rename = "R")]
+	right_speed: f32,
+}
+impl GoForward{
+	fn new( speed: f32) -> Self{
+		Self{
+			command:1,
+			left_speed: speed,
+			right_speed: speed
+		}
+	}
+}
+#[derive(Serialize)]
+pub struct GoBackward {
+	#[serde(rename = "T")]
+	command: u8, // Always 1
+	#[serde(rename = "L")]
+	left_speed: f32,
+	#[serde(rename = "R")]
+	right_speed: f32,
+}
+impl GoBackward {
+	fn new( speed: f32) -> Self{
+		Self{
+			command:1,
+			left_speed: -speed,
+			right_speed: -speed,
+		}
+	}
+}
+
+#[derive(Debug, serde::Deserialize, schemars::JsonSchema)]
+pub struct Speed{
+	#[schemars(description = " Speed of movement of Robot")]
+	speed: f32,
+}
+fn command_to_robot<T:Serialize>(command: T)-> Result<String,String>{
+	let json= serde_json::to_string(&command).unwrap();
+	let url = format!("http://{}/js?json={}", ROVER_IP, json);
+	match reqwest::blocking::get(&url) {
+		Ok(response) => Ok(response.text().unwrap_or("No response".to_string())),
+		Err(_) => Ok("Robot not responding".to_string()),
 	}
 }
 #[derive(Clone)]
@@ -37,19 +83,24 @@ impl RobotControlsServer {
 		}
 	}
 
-	#[tool(description = "command robot to move forward")]
-	async fn forward(&self,/* param: Parameters<StoreRequest>*/) -> Result<CallToolResult, ErrorData> {
-		let command = r#"{"T":1,"L":0.25,"R":0.25}"#;
-		send_command(command);
+	#[tool(description = "command robot to move forward with a given speed")]
+	async fn move_forward(&self, Parameters(Speed{speed:s}): Parameters<Speed>) -> Result<CallToolResult, ErrorData> {
+		let command=GoForward::new(s);
+		let result = command_to_robot(command);
 
-		Ok(CallToolResult::success(vec![Content::text("Moving forward ...")]))
+		Ok(CallToolResult::success(vec![Content::text(result.unwrap())]))
 	}
-	#[tool(description = "Get a memory about user")]
-	async fn stop(&self) -> Result<CallToolResult, ErrorData> {
-		let command = r#"{"T":1,"L":0,"R":0}"#;
-		send_command(command);
+	#[tool(description = "command robot to move backward with a given speed")]
+	async fn move_backward(&self, Parameters(Speed{speed:s}): Parameters<Speed>) -> Result<CallToolResult, ErrorData> {
+		let command= GoBackward::new(s);
+		let result = command_to_robot(command);
 
-		Ok(CallToolResult::success(vec![Content::text("Stopping robot ...")]))
+		Ok(CallToolResult::success(vec![Content::text(result.unwrap())]))
+	}
+
+	#[tool(description = "Command Robot to stop")]
+	async fn stop(&self) -> Result<CallToolResult, ErrorData> {
+		todo!()
 	}
 }
 
